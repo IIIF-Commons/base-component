1a89b42380ca7b3d3c799dfecf235364b587c1b4
edsilv <e.silverton@gmail.com>
2016-05-29 16:10:34 +0100

    del test

--- tasks/copy.js
+++ tasks/copy.js
@@ -3,19 +3,6 @@ var config = new c();
 var gulp = require('gulp');
 var utils = require('gulp-utils');
 
-gulp.task('copy:build', function() {
-    return gulp.src([
-        config.dist + '/' + config.jsOut,
-        config.dist + '/' + config.name + '.min.js'
-    ]).pipe(gulp.dest('./test/js'));
-});
-
-gulp.task('copy:libs', function() {
-    return gulp.src([
-        'node_modules/eventemitter2/lib/eventemitter2.js'
-    ]).pipe(gulp.dest('./test/js'));
-});
-
 gulp.task('copy:typings', function() {
     return gulp.src([
     ]).pipe(gulp.dest('./typings'));
