56b658c8fcac1a44feae42f7a53543a9498cea7e
edsilv <e.silverton@gmail.com>
2016-06-05 13:47:26 +0100

    task cleanup

--- tasks/copy.js
+++ tasks/copy.js
@@ -4,5 +4,5 @@ var gulp = require('gulp');
 var utils = require('gulp-utils');
 
 gulp.task('copy:typings', function() {
-    return gulp.src(config.typings).pipe(gulp.dest('./typings'));
+    return gulp.src(config.typings).pipe(gulp.dest(config.typingsDir));
 });
--- tasks/minify.js
+++ tasks/minify.js
@@ -1,11 +1,12 @@
 var c = require('../gulpfile.config');
 var config = new c();
 var gulp = require('gulp');
+var path = require('path');
 var utils = require('gulp-utils');
 
 gulp.task('minify', function(cb){
     Promise.all([
-        utils.minify(config.dist + '/' + config.jsOut, config.dist)
+        utils.minify(path.join(config.dist, config.jsOut), config.dist)
     ]).then(function(){
         cb();
     });
