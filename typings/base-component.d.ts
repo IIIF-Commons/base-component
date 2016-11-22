57afa61e4c324b6a1120d992da10958a0609658a
edsilv <e.silverton@gmail.com>
2016-11-22 22:30:45 +0000

    move ts definition

--- typings/base-component.d.ts
+++ typings/base-component.d.ts
@@ -1,3 +1,11 @@
 interface Window {
     _Components: any;
 }
+
+// remove if https://github.com/scottcorgan/tiny-emitter/pull/14 is merged
+interface EventEmitter {
+  on   (event: string, callback: Function, ctx?: any): EventEmitter;
+  once (event: string, callback: Function, ctx?: any): EventEmitter;
+  emit (event: string, ...args: any[]): EventEmitter;
+  off  (event: string, callback?: Function): EventEmitter;
+}
